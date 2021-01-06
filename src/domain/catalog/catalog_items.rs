5d6794c0fa026a01cf6771b090573e96c9595dc3
CarloMicieli <carlo.micieli@gmail.com>
2021-01-06 09:11:52 +0100

    Added more tests

--- src/domain/catalog/catalog_items.rs
+++ src/domain/catalog/catalog_items.rs
@@ -60,7 +60,7 @@ impl str::FromStr for PowerMethod {
         match s {
             "DC" => Ok(PowerMethod::DC),
             "AC" => Ok(PowerMethod::AC),
-            _ => Err("Invalid power method value"),
+            _ => Err("Invalid value for power methods [allowed: 'AC' or 'DC']"),
         }
     }
 }
@@ -226,6 +226,164 @@ mod tests {
         fn it_should_fail_to_convert_empty_string_slices_as_item_numbers() {
             let item_number = ItemNumber::new("");
             assert!(item_number.is_err());
+        }
+    }
+
+    mod power_method_tests {
+        use super::*;
+
+        #[test]
+        fn it_should_parse_string_as_power_methods() {
+            let pm = "AC".parse::<PowerMethod>();
+            assert!(pm.is_ok());
+            assert_eq!("AC", pm.unwrap().to_string());
+        }
+    }
+
+    mod catalog_item_tests {
+        use crate::domain::catalog::{
+            categories::{FreightCarType, LocomotiveType, PassengerCarType},
+            railways::Railway,
+            rolling_stocks::{
+                Control, DccInterface, LengthOverBuffer, ServiceLevel,
+            },
+        };
+
+        use super::*;
+
+        fn new_locomotive() -> RollingStock {
+            RollingStock::new_locomotive(
+                String::from("E.656"),
+                String::from("E.656 210"),
+                Some(String::from("1a serie")),
+                Railway::new("FS"),
+                Epoch::IV,
+                LocomotiveType::ElectricLocomotive,
+                Some(String::from("Milano Centrale")),
+                Some(String::from("blu/grigio")),
+                Some(LengthOverBuffer::new(210)),
+                Some(Control::DccReady),
+                Some(DccInterface::Nem652),
+            )
+        }
+
+        fn new_passenger_car() -> RollingStock {
+            RollingStock::new_passenger_car(
+                String::from("UIC-Z"),
+                None,
+                Railway::new("FS"),
+                Epoch::IV,
+                Some(PassengerCarType::OpenCoach),
+                Some(ServiceLevel::FirstClass),
+                None,
+                Some(String::from("bandiera")),
+                Some(LengthOverBuffer::new(303)),
+            )
+        }
+
+        fn new_freight_car() -> RollingStock {
+            RollingStock::new_freight_car(
+                String::from("Gbhs"),
+                None,
+                Railway::new("FS"),
+                Epoch::V,
+                Some(FreightCarType::SwingRoofWagon),
+                None,
+                Some(String::from("marrone")),
+                Some(LengthOverBuffer::new(122)),
+            )
+        }
+
+        fn new_locomotive_catalog_item() -> CatalogItem {
+            CatalogItem::new(
+                Brand::new("ACME"),
+                ItemNumber::new("123456").unwrap(),
+                String::from("My first catalog item"),
+                vec![new_locomotive()],
+                PowerMethod::DC,
+                Scale::from_name("H0").unwrap(),
+                1,
+            )
+        }
+
+        fn new_passenger_cars_catalog_item() -> CatalogItem {
+            CatalogItem::new(
+                Brand::new("Roco"),
+                ItemNumber::new("654321").unwrap(),
+                String::from("My first catalog item"),
+                vec![new_passenger_car(), new_passenger_car()],
+                PowerMethod::DC,
+                Scale::from_name("H0").unwrap(),
+                2,
+            )
+        }
+
+        fn new_set_catalog_item() -> CatalogItem {
+            CatalogItem::new(
+                Brand::new("ACME"),
+                ItemNumber::new("123456").unwrap(),
+                String::from("My first catalog item"),
+                vec![
+                    new_passenger_car(),
+                    new_passenger_car(),
+                    new_freight_car(),
+                ],
+                PowerMethod::DC,
+                Scale::from_name("H0").unwrap(),
+                2,
+            )
+        }
+
+        #[test]
+        fn it_should_create_new_catalog_items() {
+            let item = CatalogItem::new(
+                Brand::new("ACME"),
+                ItemNumber::new("123456").unwrap(),
+                String::from("My first catalog item"),
+                vec![new_locomotive()],
+                PowerMethod::DC,
+                Scale::from_name("H0").unwrap(),
+                1,
+            );
+
+            assert_eq!(&Brand::new("ACME"), item.brand());
+            assert_eq!(&ItemNumber::new("123456").unwrap(), item.item_number());
+            assert_eq!("My first catalog item", item.description());
+            assert_eq!(&vec![new_locomotive()], item.rolling_stocks());
+            assert_eq!(PowerMethod::DC, item.power_method());
+            assert_eq!(&Scale::from_name("H0").unwrap(), item.scale());
+            assert_eq!(1, item.count());
+        }
+
+        #[test]
+        fn it_should_check_whether_catalog_item_is_a_locomotive() {
+            let item = new_locomotive_catalog_item();
+            assert!(true, item.is_locomotive());
+        }
+
+        #[test]
+        fn it_should_extract_the_category_from_catalog_items() {
+            let item1 = new_locomotive_catalog_item();
+            let item2 = new_passenger_cars_catalog_item();
+
+            assert_eq!(Category::Locomotives, item1.category());
+            assert_eq!(Category::PassengerCars, item2.category());
+        }
+
+        #[test]
+        fn it_should_produce_string_representations_from_catalog_items() {
+            let item = new_locomotive_catalog_item();
+            assert_eq!("ACME 123456 (L)", item.to_string());
+        }
+
+        #[test]
+        fn it_should_check_whether_two_catalog_items_are_equal() {
+            let item1 = new_locomotive_catalog_item();
+            let item2 = new_locomotive_catalog_item();
+            let item3 = new_passenger_cars_catalog_item();
+
+            assert!(item1 == item2);
+            assert!(item1 != item3);
         }
     }
 }
